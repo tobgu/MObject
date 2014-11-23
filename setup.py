d0fa49ecc0bcfad5a8d4ee524bc4670b3555c006
Tobias Gustafsson <tobias.l.gustafsson@gmail.com>
2014-11-23 18:13:51 +0100

    Testing with tox and python 2/3 compatibility

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='mobject',
+      version='0.1',
+      description='Object tree factory',
+      url='http://github.com/tobgu/mobject',
+      author='Tobias Gustafsson',
+      author_email='tobias.l.gustafsson@gmail.com',
+      license='LICENCE.mit',
+      py_modules=['mobject'],
+      use_2to3 = True,
+      zip_safe=False)
